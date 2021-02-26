a2d2dfe4d2128ce446b9f2b8eb18a622fde27196
Antti Kangas <antti.kangas99@gmail.com>
2021-02-26 20:35:32 +0200

    Fix port usage on Heroku

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get("/availability/", (req, res) => {
     res.send("Availability")
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
