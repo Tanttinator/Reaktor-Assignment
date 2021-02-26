4949715601199d953f85c0c6295f6ddbd318e9ad
Antti Kangas <antti.kangas99@gmail.com>
2021-02-26 20:25:09 +0200

    Add communication with backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios"
+import { useEffect, useState } from "react";
+
+const productsURL = "http://localhost:3001/products/"
+const availabilityURL = "http://localhost:3001/availability/"
 
 function App() {
+
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(productsURL)
+      .then(response => {
+        setItems(response.data)
+      }).catch(error => console.log(error))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Title</p>
+      {items.map(item => <p>{item.name}</p>)}
     </div>
   );
 }
