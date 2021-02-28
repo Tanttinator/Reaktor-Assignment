f4215ff32369126b210f6c7ee2053122f76098d3
Antti Kangas <antti.kangas99@gmail.com>
2021-02-28 20:57:05 +0200

    Store data of product availability

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors")
 
 //Store the URLs used by the app
 const productsURL = "https://bad-api-assignment.reaktor.com/v2/products/"
-const availabilityURL = "https://bad-api-assignment.reaktor.com/v2/availability"
+const availabilityURL = "https://bad-api-assignment.reaktor.com/v2/availability/"
 
 //Cache for data on the assignment API
 var products = {}
@@ -17,34 +17,60 @@ app.use(cors())
 //Serve our frontend to clients
 app.use(express.static("build"))
 
+//Get the availability of a given product
+const getAvailability = (product) => {
+    return new Promise((resolve, reject) => {
+        dataService(manufacturers, product.manufacturer, availabilityURL)
+            .then(manufacturerData => {
+                const productData = manufacturerData.response.find(p => p.id === product.id.toUpperCase())
+                if(productData !== undefined) {
+                    const inStock = productData.DATAPAYLOAD.split("<INSTOCKVALUE>")[1].split("</INSTOCKVALUE>")[0]
+                    if(inStock === "INSTOCK") resolve("In stock")
+                    else if(inStock === "OUTOFSTOCK") resolve("Out of stock")
+                    else if(inStock === "LESSTHAN10") resolve("Less than 10")
+                    else resolve("Corrupt data")
+                } else {
+                    resolve("No data")
+                }
+            }).catch(reject)
+    })
+}
+
 //Handles data requests made by frontend
-const dataService = (cache, param, url, res) => {
+const dataService = (cache, param, url) => {
     if(param in cache && Date.now() - cache[param].timestamp < 300000) { //If the requested data is found in the cache and it is less than 5 minutes old, send it
-        res.json(cache[param].data)
         console.log(`${param} found in cache`)
-    } else {            //Else fetch the data from the API, store it in the cache and send it
+        return Promise.resolve(cache[param].data)
+    } else {                                                            //Else fetch the data from the API, store it in the cache and send it
         console.log(`Up-to-date data for ${param} not found in cache. Fetching new data...`)
         const startTime = Date.now()
-        axios
-        .get(`${url}${param}`)
-        .then(response => {
-            cache[param] = { data: response.data, timestamp: Date.now() }
-            res.json(response.data)
-            console.log(`Fetched data in ${Date.now() - startTime}ms`)
-        })
-        .catch(error => {
-            console.log(error.message)
-            res.json({type: "error", msg: error.message})
+        return new Promise((resolve, reject) => {
+            axios
+                .get(`${url}${param}`)
+                .then(response => {
+                    cache[param] = { data: response.data, timestamp: Date.now() }
+                    console.log(`Fetched data in ${Date.now() - startTime}ms`)
+                    resolve(response.data)
+                })
         })
     }
 }
 
-app.get("/products/:category", (req, res) => {
-    dataService(products, req.params.category, productsURL, res)
-})
-
-app.get("/availability/:manufacturer", (req, res) => {
-    dataService(manufacturers, req.params.manufacturer, availabilityURL, res)
+app.get("/products/:category", async (req, res) => {
+    try {
+        const data = await dataService(products, req.params.category, productsURL, res)
+        if(data.type !== "error") {
+            for(let product of data) {
+                product.availability = await getAvailability(product).catch(error => { throw error })
+            }
+            res.json(data)
+        } else {
+            res.json(data)
+        }
+    } catch(error) {
+        console.log(error)
+        res.json(error)
+    }
 })
 
 const PORT = process.env.PORT || 3001
