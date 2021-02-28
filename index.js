a0b79e5d0517dcbb3d7368022afef8c3cb5f2bfe
Antti Kangas <antti.kangas99@gmail.com>
2021-02-28 17:46:10 +0200

    Keep data up-to-date in the cache

--- index.js
+++ index.js
@@ -19,16 +19,16 @@ app.use(express.static("build"))
 
 //Handles data requests made by frontend
 const dataService = (cache, param, url, res) => {
-    if(param in cache) { //If the requested data is found in the cache, send it
-        res.json(cache[param])
+    if(param in cache && Date.now() - cache[param].timestamp < 300000) { //If the requested data is found in the cache and it is less than 5 minutes old, send it
+        res.json(cache[param].data)
         console.log(`${param} found in cache`)
     } else {            //Else fetch the data from the API, store it in the cache and send it
-        console.log(`${param} not found in cache. Fetching data...`)
+        console.log(`Up-to-date data for ${param} not found in cache. Fetching new data...`)
         const startTime = Date.now()
         axios
         .get(`${url}${param}`)
         .then(response => {
-            cache[param] = response.data
+            cache[param] = { data: response.data, timestamp: Date.now() }
             res.json(response.data)
             console.log(`Fetched data in ${Date.now() - startTime}ms`)
         })
